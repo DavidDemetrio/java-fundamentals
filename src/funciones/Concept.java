f3bb4d8061dfc584a59329679ed49e70ce8409eb
DavidDemetrio <davidlopez.paz26@outlook.es>
2024-09-04 23:28:37 -0600

    Functions in java

---
+++ src/funciones/Concept.java
@@ -0,0 +1,13 @@
+package funciones;
+
+public class Concept {
+    public static void main(String[] args) {
+        /**
+         * Definición de funciones 
+         * 1 Bloque de código Reutilizable
+         * 2 Cumple una tarea en específico
+         * 3 Puede tener parametros de entrada
+         * 3 Puede devolver un dato de salida.
+        */
+    }
+}
